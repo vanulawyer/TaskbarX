a6e3c5756d1ca288a276fa2f86ed7b299323f3e4
vanulawyer <vanulawyer@gmail.com>
2020-05-17 05:39:25 +1000

    abc
    123

---
+++ TaskbarXInstaller/Form1.Designer.vb
@@ -0,0 +1,186 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(120, Byte), Integer), CType(CType(215, Byte), Integer))
+        Me.Button1.FlatAppearance.BorderSize = 0
+        Me.Button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(51, Byte), Integer), CType(CType(147, Byte), Integer), CType(CType(223, Byte), Integer))
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(354, 185)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(117, 35)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Install"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 154)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(459, 23)
+        Me.ProgressBar1.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(124, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(86, 25)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "TaskbarX"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(126, 61)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(150, 15)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Publisher: Chris Andriessen"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(9, 136)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(12, 15)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "-"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(126, 80)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(179, 15)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Website: www.chrisandriessen.nl"
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(14, 12)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(100, 100)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox2.TabIndex = 7
+        Me.PictureBox2.TabStop = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.DimGray
+        Me.Button2.FlatAppearance.BorderSize = 0
+        Me.Button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(51, Byte), Integer), CType(CType(147, Byte), Integer), CType(CType(223, Byte), Integer))
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.White
+        Me.Button2.Location = New System.Drawing.Point(76, 185)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(117, 35)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "Uninstall"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.DimGray
+        Me.Button3.FlatAppearance.BorderSize = 0
+        Me.Button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(51, Byte), Integer), CType(CType(147, Byte), Integer), CType(CType(223, Byte), Integer))
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.ForeColor = System.Drawing.Color.White
+        Me.Button3.Location = New System.Drawing.Point(12, 185)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(58, 35)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "Close"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(126, 38)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(63, 17)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Version: -"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(483, 232)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "TaskbarX Installer"
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label5 As Label
+End Class
---
+++ TaskbarXInstaller/Form1.vb
@@ -0,0 +1,231 @@
+﻿Imports System.Environment
+Imports System.IO
+Imports Microsoft.Win32.TaskScheduler
+
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        install()
+    End Sub
+
+    Public Sub install()
+        Dim appData As String = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
+        Dim programfiles = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
+        Dim path As String = programfiles & "\TaskbarX"
+        Button1.Enabled = False
+
+        ProgressBar1.Value = 0
+        Label3.Text = "Stopping TaskbarX..."
+        Shell("taskkill /F /IM TaskbarX.exe")
+        Shell("taskkill /F /IM " & Chr(34) & "TaskbarX Configurator.exe" & Chr(34))
+        ProgressBar1.Value = ProgressBar1.Value + 10
+        System.Threading.Thread.Sleep(20) : Application.DoEvents()
+
+        Try
+            If Directory.Exists(path) Then
+                System.IO.Directory.Delete(path, True)
+            End If
+        Catch
+        End Try
+
+        Directory.CreateDirectory(path)
+
+        For Each Filex In Directory.GetFiles(Application.StartupPath)
+            Dim filename = Filex.Replace(Application.StartupPath, "")
+            Try
+                File.Copy(Filex, path & "\" & filename)
+            Catch
+            End Try
+            ProgressBar1.Value = ProgressBar1.Value + 1
+            Label3.Text = "Installing " & filename & "..."
+            System.Threading.Thread.Sleep(10) : Application.DoEvents()
+        Next
+
+        Try
+            CreateShortCut(path & "\TaskbarX.exe", FileIO.SpecialDirectories.Desktop, "TaskbarX")
+            CreateShortCut(path & "\TaskbarX Configurator.exe", FileIO.SpecialDirectories.Desktop, "TaskbarX Configurator")
+        Catch
+        End Try
+
+        Try
+            If Directory.Exists(appData & "\Microsoft\Windows\Start Menu\Programs\Chris Andriessen") Then
+                System.IO.Directory.Delete(appData & "\Microsoft\Windows\Start Menu\Programs\Chris Andriessen", True)
+            End If
+        Catch
+        End Try
+
+        Try
+            Directory.CreateDirectory(appData & "\Microsoft\Windows\Start Menu\Programs\TaskbarX")
+            CreateShortCut(path & "\TaskbarX.exe", appData & "\Microsoft\Windows\Start Menu\Programs\TaskbarX", "TaskbarX")
+            CreateShortCut(path & "\TaskbarX Configurator.exe", appData & "\Microsoft\Windows\Start Menu\Programs\TaskbarX", "TaskbarX Configurator")
+        Catch
+        End Try
+        '  Console.WriteLine(appData)
+
+        Dim FileVer As String = FileVersionInfo.GetVersionInfo(path & "\TaskbarX.exe").FileVersion
+
+        Console.WriteLine(FileVer)
+
+        'Setting My Values
+        Dim ApplicationName As String = "TaskbarX"
+        Dim ApplicationVersion As String = FileVer
+        Dim ApplicationIconPath As String = "%APPDATA%\TaskbarX\TaskbarX-Install-100.ico"
+        Dim ApplicationPublisher As String = "Chris Andriessen"
+        Dim ApplicationUnInstallPath As String = "%APPDATA%\TaskbarX\TaskbarXInstaller.exe"
+        Dim ApplicationInstallDirectory As String = "%APPDATA%\TaskbarX\"
+
+        ProgressBar1.Value = ProgressBar1.Value + 10
+        Label3.Text = "Creating uninstall entry..."
+        System.Threading.Thread.Sleep(10) : Application.DoEvents()
+
+        Try
+            'Openeing the Uninstall RegistryKey (don't forget to set the writable flag to true)
+            With My.Computer.Registry.CurrentUser.OpenSubKey("Software\Microsoft\Windows\CurrentVersion\Uninstall", True)
+
+                'Creating my AppRegistryKey
+                Dim AppKey As Microsoft.Win32.RegistryKey = .CreateSubKey(ApplicationName)
+
+                'Adding my values to my AppRegistryKey
+                AppKey.SetValue("DisplayName", ApplicationName, Microsoft.Win32.RegistryValueKind.String)
+                AppKey.SetValue("DisplayVersion", ApplicationVersion, Microsoft.Win32.RegistryValueKind.String)
+                AppKey.SetValue("DisplayIcon", ApplicationIconPath, Microsoft.Win32.RegistryValueKind.String)
+                AppKey.SetValue("Publisher", ApplicationPublisher, Microsoft.Win32.RegistryValueKind.String)
+                AppKey.SetValue("UninstallString", ApplicationUnInstallPath, Microsoft.Win32.RegistryValueKind.String)
+                AppKey.SetValue("UninstallPath", ApplicationUnInstallPath, Microsoft.Win32.RegistryValueKind.String)
+                AppKey.SetValue("InstallLocation", ApplicationInstallDirectory, Microsoft.Win32.RegistryValueKind.String)
+
+            End With
+        Catch
+        End Try
+
+        ProgressBar1.Value = 100
+        Label3.Text = "Done!"
+
+        Process.Start(path & "\TaskbarX.exe")
+        Process.Start(path & "\TaskbarX Configurator.exe")
+
+        Button1.Enabled = True
+        Button2.Enabled = True
+        Button1.Text = "Update"
+    End Sub
+
+    Private Function CreateShortCut(ByVal TargetName As String, ByVal ShortCutPath As String, ByVal ShortCutName As String) As Boolean
+        Dim oShell As Object
+        Dim oLink As Object
+        'you don’t need to import anything in the project reference to create the Shell Object
+
+        Try
+
+            oShell = CreateObject("WScript.Shell")
+            oLink = oShell.CreateShortcut(ShortCutPath & "\" & ShortCutName & ".lnk")
+
+            oLink.TargetPath = TargetName
+            oLink.WindowStyle = 1
+            oLink.Save()
+        Catch ex As Exception
+
+        End Try
+
+    End Function
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ProgressBar1.Value = 0
+
+        Label3.Text = "Stopping TaskbarX..."
+        System.Threading.Thread.Sleep(20) : Application.DoEvents()
+
+        Dim programfiles = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
+        Dim path As String = programfiles & "\TaskbarX"
+
+        Process.Start(path & "\TaskbarX.exe", "-stop")
+
+        System.Threading.Thread.Sleep(1000) : Application.DoEvents()
+        Shell("taskkill /F /IM TaskbarX.exe")
+        Shell("taskkill /F /IM " & Chr(34) & "TaskbarX Configurator.exe" & Chr(34))
+        ProgressBar1.Value = ProgressBar1.Value + 20
+        System.Threading.Thread.Sleep(20) : Application.DoEvents()
+        System.Threading.Thread.Sleep(1000) : Application.DoEvents()
+
+        Label3.Text = "Removing Files..."
+        Try
+            If Directory.Exists(path) Then
+                System.IO.Directory.Delete(path, True)
+            End If
+        Catch
+        End Try
+
+        Dim appData As String = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
+        ProgressBar1.Value = ProgressBar1.Value + 20
+        Label3.Text = "Removing Shortcuts..."
+        System.Threading.Thread.Sleep(20) : Application.DoEvents()
+
+        Try
+            If Directory.Exists(appData & "\Microsoft\Windows\Start Menu\Programs\Chris Andriessen") Then
+                System.IO.Directory.Delete(appData & "\Microsoft\Windows\Start Menu\Programs\Chris Andriessen", True)
+            End If
+            If Directory.Exists(appData & "\Microsoft\Windows\Start Menu\Programs\TaskbarX") Then
+                System.IO.Directory.Delete(appData & "\Microsoft\Windows\Start Menu\Programs\TaskbarX", True)
+            End If
+        Catch
+        End Try
+
+        ProgressBar1.Value = ProgressBar1.Value + 20
+        Label3.Text = "Removing Taskschedule..."
+        System.Threading.Thread.Sleep(20) : Application.DoEvents()
+        Try
+            Using ts As TaskService = New TaskService()
+                ts.RootFolder.DeleteTask("TaskbarX")
+                ts.RootFolder.DeleteTask("FalconX")
+            End Using
+        Catch ex As Exception
+            Console.WriteLine(ex.Message)
+        End Try
+
+        ProgressBar1.Value = ProgressBar1.Value + 20
+        ' Label3.Text = "Removing Taskschedule..."
+        System.Threading.Thread.Sleep(20) : Application.DoEvents()
+
+        Try
+            With My.Computer.Registry.CurrentUser.OpenSubKey("Software\Microsoft\Windows\CurrentVersion\Uninstall", True)
+                .DeleteSubKey("TaskbarX")
+            End With
+        Catch
+        End Try
+
+        ProgressBar1.Value = 100
+        System.Threading.Thread.Sleep(20) : Application.DoEvents()
+        Label3.Text = "Uninstalled!"
+
+        Button1.Enabled = True
+        Button2.Enabled = False
+        Button1.Text = "Install"
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim newFileVer As String = FileVersionInfo.GetVersionInfo(Application.StartupPath & "\TaskbarX.exe").FileVersion
+        Label5.Text = "Version: " & newFileVer
+
+        Dim programfiles = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
+        Dim path As String = programfiles & "\TaskbarX\TaskbarX.exe"
+
+        Try
+            If File.Exists(path) Then
+                Dim curFileVer As String = FileVersionInfo.GetVersionInfo(path).FileVersion
+                Button1.Text = "Update"
+                Label3.Text = "TaskbarX " & curFileVer & " is currently installed."
+            Else
+                Button1.Enabled = True
+                Button2.Enabled = False
+                Label3.Text = "TaskbarX is currently not installed."
+            End If
+        Catch
+        End Try
+
+    End Sub
+
+End Class
